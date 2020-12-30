ae04a96ca3be4f46143688f3983a3a40011cc017
Miaopashi <1246555055@qq.com>
2020-12-30 14:47:48 +0800

    test

---
+++ src/main/java/xyz/yysy/salary/Controller.java
@@ -0,0 +1,11 @@
+package xyz.yysy.salary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
