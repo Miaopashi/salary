d478508048fa409ee1756268567b6973f3f4cf5f
Miaopashi <1246555055@qq.com>
2020-12-30 21:50:50 +0800

    restful控制器写了个bug...

---
+++ src/main/java/xyz/yysy/salary/controller/AjaxController.java
@@ -0,0 +1,26 @@
+package xyz.yysy.salary.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.yysy.salary.model.Respondent;
+import xyz.yysy.salary.repository.RespondentRepository;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(path = "/data", produces = "applicaiton/json")
+@CrossOrigin("*")
+public class AjaxController {
+    private RespondentRepository respondentRepo;
+
+    public AjaxController(RespondentRepository respondentRepo) {
+        this.respondentRepo = respondentRepo;
+    }
+
+    @GetMapping("/all")
+    public Iterable<Respondent> allRespondents() {
+        return respondentRepo.findAll();
+    }
+}
--- src/main/java/xyz/yysy/salary/repository/RespondentRepository.java
+++ src/main/java/xyz/yysy/salary/repository/RespondentRepository.java
@@ -1,7 +1,10 @@
 package xyz.yysy.salary.repository;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import xyz.yysy.salary.model.Respondent;
+
+import java.net.URLConnection;
 
 public interface RespondentRepository extends CrudRepository<Respondent, String> {
 }
