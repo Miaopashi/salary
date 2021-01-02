91e0a903a366abff38e197548583b286ed236b26
Miaopashi <05181808@cumt.edu.cn>
2021-01-02 15:09:29 +0800

    restful请求有点难搞

--- src/main/java/xyz/yysy/salary/controller/AjaxController.java
+++ src/main/java/xyz/yysy/salary/controller/AjaxController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import xyz.yysy.salary.model.Respondent;
 import xyz.yysy.salary.repository.RespondentRepository;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
-//@RequestMapping(path = "/data", produces = "applicaiton/json")
+//@RequestMapping(path = "/data", produces = {"applicaiton/json", "text/html"})
 @CrossOrigin("*")
 public class AjaxController {
     private RespondentRepository respondentRepo;
@@ -20,7 +20,70 @@ public class AjaxController {
     }
 
     @GetMapping("/all")
-    public Iterable<Respondent> allRespondents() {
-        return respondentRepo.findAll();
+    public HashMap<String, ArrayList<Integer>> allRespondents() {
+        Iterable<Respondent> allRespondents = respondentRepo.findAll();
+        // 根据薪水分为4层
+        double maxSalary = allRespondents.iterator().next().getSalary();
+        double minSalary = allRespondents.iterator().next().getSalary();
+        for (Respondent r :
+                allRespondents) {
+            if (r.getSalary() > maxSalary)
+                maxSalary = r.getSalary();
+            if (r.getSalary() < minSalary)
+                minSalary = r.getSalary();
+        }
+        ArrayList<Double> salaryLayer = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            salaryLayer.add((maxSalary - minSalary) * (double) i / (double) 4 + minSalary);
+        }
+
+//        ArrayList<Double> salaryLayer = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            salaryLayer.add((double) 0);
+//        }
+//        ArrayList<Double> salaryList = new ArrayList<>();
+//        for (Respondent r :
+//                allRespondents) {
+//            salaryList.add(r.getSalary());
+//        }
+//        Collections.sort(salaryList);
+//        int size = salaryList.size();
+//        salaryLayer.set(0, salaryList.get(0));
+//        if (size % 4 != 1) {
+//            salaryLayer.set(1, (salaryList.get(size/4 - 1) + salaryList.get(size/4)) / 2);
+//            if (size % 2 != 1)
+//                salaryLayer.set(2, (salaryList.get(size/2 - 1) + salaryList.get(size/2)) / 2);
+//            else
+//                salaryLayer.set(2, salaryList.get(size/2 - 1) / 2);
+//            salaryLayer.set(3, (salaryList.get(size*3/4 - 1) + salaryList.get(size*3/4)) / 2);
+//        } else {
+//            salaryLayer.set(1, salaryList.get(size/4 - 1) / 2);
+//            salaryLayer.set(2, salaryList.get(size/2 - 1) / 2);
+//            salaryLayer.set(3, salaryList.get(size*3/4 - 1) / 2);
+//        }
+//        salaryLayer.set(4, salaryList.get(size-1));
+
+        ArrayList<Integer> fNum = new ArrayList<>();
+        ArrayList<Integer> mNum = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            int fCount, mCount;
+            fCount = mCount = 0;
+            for (Respondent r :
+                    allRespondents) {
+                double salary = r.getSalary();
+                if (salary >= salaryLayer.get(i) && salary <= salaryLayer.get(i+1)) {
+                    if (r.getGender().equals("f"))
+                        fCount++;
+                    else
+                        mCount++;
+                }
+            }
+            fNum.add(fCount);
+            mNum.add(mCount);
+        }
+        HashMap<String, ArrayList<Integer>> result = new HashMap<>();
+        result.put("fNum", fNum);
+        result.put("mNum", mNum);
+        return result;
     }
 }
