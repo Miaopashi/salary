654875b2b4e0b3a6dd27c264c89a1e4a1cbfa63e
Miaopashi <05181808@cumt.edu.cn>
2021-01-03 11:00:14 +0800

    完成了第二个图的后端接口

--- src/main/java/xyz/yysy/salary/controller/AjaxController.java
+++ src/main/java/xyz/yysy/salary/controller/AjaxController.java
@@ -38,4 +38,59 @@ public class AjaxController {
         result.put("data_salary", allSalary);
         return result;
     }
+
+    @GetMapping("/grade_chart")
+    public ArrayList<ArrayList<Double>> gradeChart() {
+        Iterable<Respondent> all = respondentRepo.findAll();
+
+        // 根据薪资分为4层
+        ArrayList<Double> layer = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            layer.add((double) 0);
+        }
+        ArrayList<Double> salary = new ArrayList<>();
+        for (Respondent r :
+                all) {
+            salary.add(r.getSalary());
+        }
+        Collections.sort(salary);
+        int size = salary.size();
+        layer.set(0, salary.get(0));
+        if (size % 4 != 1) {
+            layer.set(1, (salary.get(size/4 - 1) + salary.get(size/4)) / 2);  // 下四分位点
+            if (size % 2 != 1)
+                layer.set(2, (salary.get(size/2 - 1) + salary.get(size/2)) / 2);
+            else
+                layer.set(2, salary.get(size/2 - 1));
+            layer.set(3, (salary.get(size*3/4 - 1) + salary.get((size*3/4)) / 2));  // 上四分位点
+        } else {
+            layer.set(1, salary.get(size/4 - 1));  // 下四分位点
+            layer.set(2, salary.get(size/2 - 1));
+            layer.set(3, salary.get(size*3/4 - 1));  // 上四分位点
+        }
+        layer.set(4, salary.get(size - 1));
+
+        // 统计各层的数据
+        int gpaCount, perCount;
+        double gpaSum, perSum;
+        ArrayList<ArrayList<Double>> result = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            ArrayList<Double> avg = new ArrayList<>();
+            gpaCount = perCount = 0;
+            gpaSum = perSum = 0;
+            for (Respondent r :
+                    all) {
+                if (r.getSalary() >= layer.get(i) && r.getSalary() < layer.get(i + 1)) {
+                    gpaCount++;
+                    gpaSum += r.getCollegeGPA();
+                    perCount++;
+                    perSum += r.getPercentage12();
+                }
+            }
+            avg.add(perSum / perCount);
+            avg.add(gpaSum / gpaCount);
+            result.add(avg);
+        }
+        return result;
+    }
 }
--- src/test/java/xyz/yysy/salary/SalaryApplicationTests.java
+++ src/test/java/xyz/yysy/salary/SalaryApplicationTests.java
@@ -8,6 +8,7 @@ class SalaryApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(1+1);
     }
 
 }
